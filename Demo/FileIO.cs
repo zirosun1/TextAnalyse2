5f945f40e809be62283d96eb442e67ef62f09507
zirosun1 <haneange@gmail.com>
2018-05-24 12:05:52 +0300

    Initial commit

---
+++ Demo/FileIO.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Demo
+{
+   static class FileIO
+    {
+        public static string ReadFile(string path)
+        {
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    return sr.ReadToEnd();
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+        public static void WriteFile(string path, string data)
+        {
+            try
+            {
+                using (StreamWriter sw = File.AppendText(path))
+                {
+                    sw.Write(data);
+                }
+            }
+            catch(IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextAnalyse;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Properties.Settings.Default.path;
+            string writePath = Properties.Settings.Default.writePath;
+            string result = Analizator.AnaliseText(FileIO.ReadFile(path));
+            FileIO.WriteFile(writePath, result);
+            Console.ReadLine();
+        }
+    }
+}
